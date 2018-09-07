fc2acab26b298d53158c5a1a829552761c7a23e1
mkaian <24683835+mkaian@users.noreply.github.com>
2018-09-06 23:52:50 -0400

    First commit

--- 5 Databases and SQL for Data Science/PETSALE-FUNCTIONS.sql
+++ 5 Databases and SQL for Data Science/PETSALE-FUNCTIONS.sql
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 select SUM(SALEPRICE) from PETSALE;
 select SUM(SALEPRICE) AS SUM_OF_SALEPRICE from PETSALE;
 select MAX(QUANTITY) from PETSALE;
@@ -13,3 +14,20 @@ select COUNT(*) from PETSALE where MONTH(SALEDATE)='05';
 select (SALEDATE + 3 DAYS) from PETSALE;
 select (CURRENT DATE - SALEDATE) from PETSALE;
 
+=======
+select SUM(SALEPRICE) from PETSALE;
+select SUM(SALEPRICE) AS SUM_OF_SALEPRICE from PETSALE;
+select MAX(QUANTITY) from PETSALE;
+select AVG(SALEPRICE) from PETSALE;
+select AVG( SALEPRICE / QUANTITY ) from PETSALE where ANIMAL = 'Dog';
+select ROUND(SALEPRICE) from PETSALE;
+select LENGTH(ANIMAL) from PETSALE;
+select UCASE(ANIMAL) from PETSALE;
+select DISTINCT(UCASE(ANIMAL)) from PETSALE;
+select * from PETSALE where LCASE(ANIMAL) = 'cat';
+select DAY(SALEDATE) from PETSALE where ANIMAL = 'Cat';
+select COUNT(*) from PETSALE where MONTH(SALEDATE)='05';
+select (SALEDATE + 3 DAYS) from PETSALE;
+select (CURRENT DATE - SALEDATE) from PETSALE;
+
+>>>>>>> fd6a1e341dcc704512add46e36e646d33ed275ea
